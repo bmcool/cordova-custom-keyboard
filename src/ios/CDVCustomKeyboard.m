53be71ae368d5bba2a04ae3025e7903782d1a073
bmcool <bloggerbm@gmail.com>
2016-08-22 23:48:07 +0800

    custom keyboard

--- src/ios/CDVCustomKeyboard.m
+++ src/ios/CDVCustomKeyboard.m
@@ -1,6 +1,29 @@
 #import "CDVCustomKeyboard.h"
 
-@interface CDVCustomKeyboard ()<UITextViewDelegate>
+@implementation UIView (FindFirstResponder)
+- (UIView *)findFirstResponder
+{
+    if (self.isFirstResponder) {
+        return self;
+    }
+    
+    for (UIView *subView in self.subviews) {
+        UIView *firstResponder = [subView findFirstResponder];
+        
+        if (firstResponder != nil) {
+            return firstResponder;
+        }
+    }
+    
+    return nil;
+}
+@end
+
+
+@interface CDVCustomKeyboard ()<UITextViewDelegate> {
+    BOOL isBlockHtmlKeyboard;
+    NSString *elementId;
+}
 
 @property (nonatomic, readwrite, assign) BOOL keyboardIsVisible;
 
@@ -13,6 +36,7 @@ UITextView *hiddenTextView;
 - (void)pluginInitialize
 {
     if (hiddenTextView == NULL) {
+        isBlockHtmlKeyboard = NO;
         hiddenTextView = [[UITextView alloc] init];
         hiddenTextView.alpha = 0;
         hiddenTextView.autocorrectionType = UITextAutocorrectionTypeNo;
@@ -20,85 +44,90 @@ UITextView *hiddenTextView;
         hiddenTextView.keyboardType = UIKeyboardTypeDecimalPad;
         hiddenTextView.delegate = self;
         [self.viewController.view addSubview:hiddenTextView];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     }
 }
 
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    if (!isBlockHtmlKeyboard) {
+        return;
+    }
+    
+    UIView * v = [self.webView findFirstResponder];
+    
+    if (v) {
+        [(UIWebView *)self.webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.getElementById('%@').keyboard = document.activeElement;", elementId]];
+        
+        hiddenTextView.text = [(UIWebView *)self.webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.getElementById('%@').keyboard.value;", elementId]];
+        [hiddenTextView becomeFirstResponder];
+        UIButton *btn = [[UIButton alloc] initWithFrame:self.viewController.view.frame];
+        [btn addTarget:self action:@selector(resignKeyBoard:) forControlEvents:UIControlEventTouchUpInside];
+        [self.viewController.view addSubview:btn];
+    }
+}
+
+-(void) resignKeyBoard:(UIButton *)sender
+{
+    [hiddenTextView resignFirstResponder];
+    [sender removeFromSuperview];
+    isBlockHtmlKeyboard = NO;
+}
+
 - (void)textViewDidChange:(UITextView *)textView {
-    NSString *text = textView.text;
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:text];
-    [pluginResult setKeepCallbackAsBool:YES];
-    [self.commandDelegate sendPluginResult:pluginResult  callbackId:self.callbackId];
+    [(UIWebView *)self.webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.getElementById('%@').keyboard.value = '%@';", elementId, hiddenTextView.text]];
 }
 
 - (void) textViewDidEndEditing:(UITextView *)textView {
-    NSString *text = textView.text;
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:text];
-    [self.commandDelegate sendPluginResult:pluginResult  callbackId:self.callbackId];
+    NSLog(@"textViewDidEndEditing");
 }
 
 - (void)open:(CDVInvokedUrlCommand*)command
 {
-    self.callbackId = command.callbackId;
-    NSString *startedValue = [command argumentAtIndex:0];
+    isBlockHtmlKeyboard = YES;
+    
+    elementId = [command argumentAtIndex:0];
     NSInteger keyBoardTypeInt = [[command argumentAtIndex:1] integerValue];
     
     switch (keyBoardTypeInt) {
         case 1:
-            self.keyboardType =  UIKeyboardTypeDefault;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeDefault;
             break;
         case 2:
-            self.keyboardType =  UIKeyboardTypeASCIICapable;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeASCIICapable;
             break;
         case 3:
-            self.keyboardType =  UIKeyboardTypeNumbersAndPunctuation;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeNumbersAndPunctuation;
             break;
         case 4:
-            self.keyboardType =  UIKeyboardTypeURL;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeURL;
             break;
         case 5:
-            self.keyboardType =  UIKeyboardTypeNumberPad;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeNumberPad;
             break;
         case 6:
-            self.keyboardType =  UIKeyboardTypePhonePad;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypePhonePad;
             break;
         case 7:
-            self.keyboardType =  UIKeyboardTypeNamePhonePad;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeNamePhonePad;
             break;
         case 8:
-            self.keyboardType =  UIKeyboardTypeEmailAddress;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeEmailAddress;
             break;
         case 9:
-            self.keyboardType =  UIKeyboardTypeDecimalPad;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeDecimalPad;
             break;
         case 10:
-            self.keyboardType =  UIKeyboardTypeTwitter;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeTwitter;
             break;
         case 11:
-            self.keyboardType =  UIKeyboardTypeWebSearch;                // Default type for the current input method.
+            self.keyboardType =  UIKeyboardTypeWebSearch;
             break;
         default:
             self.keyboardType =  keyBoardTypeInt;
             break;
     }
     hiddenTextView.keyboardType = self.keyboardType;
-    hiddenTextView.text = startedValue;
-    [hiddenTextView becomeFirstResponder];
 }
-
-- (void)close:(CDVInvokedUrlCommand*)command
-{
-    [self.webView becomeFirstResponder];
-    [hiddenTextView resignFirstResponder];
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK]  callbackId:command.callbackId];
-}
-
-- (void)change:(CDVInvokedUrlCommand*)command
-{
-    NSString *value = [command argumentAtIndex:0];
-    hiddenTextView.text = value;
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
 
 @end
